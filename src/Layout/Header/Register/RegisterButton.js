f09d99f3ea88b5189549fcf65cee23e02f3382fb
Son-MBP13 <tson0406@gmail.com>
2020-06-15 16:47:40 +0700

    add header, logo, searchbox, register, login, cart

---
+++ src/Layout/Header/Register/RegisterButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./RegisterButton.scss";
+
+const RegisterButton = (props) => {
+    return (
+        <div className="register-button">
+            <span className="greyish-brown">Register</span>
+        </div>
+    );
+};
+
+export default RegisterButton;
---
+++ src/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from './Header/Header'
+
+const layout = (props) => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+export default layout;
