f8f28ac6ee89205b23a9fe07e45ecde40ec3728d
Son-MBP13 <tson0406@gmail.com>
2020-06-16 16:17:59 +0700

    add router

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,6 +1,10 @@
 import React from "react";
+
+import { Switch, Route } from "react-router-dom";
+
 import Header from "./Header/Header";
 import NavBar from "./NavBar/NavBar";
+import routes from "../router";
 
 import Footer from "./Footer/Footer";
 const layout = (props) => {
@@ -8,6 +12,16 @@ const layout = (props) => {
         <div>
             <Header />
             <NavBar />
+            <Switch>
+                {routes.map((route, i) => (
+                    <Route
+                        key={i}
+                        path={route.path}
+                        // exact={route.exact}
+                        children={<route.component />}
+                    />
+                ))}
+            </Switch>
             <Footer />
         </div>
     );
