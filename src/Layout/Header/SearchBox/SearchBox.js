9930d845a5b82aab24fd116906f89e2006064864
Son-MBP13 <tson0406@gmail.com>
2020-06-16 11:04:46 +0700

    Refactor Navbar -> react list rendering #5

--- src/Layout/Header/SearchBox/SearchBox.js
+++ src/Layout/Header/SearchBox/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = (props) => {
     return (
         <div className="container-search-box">
             <input type="text" placeholder="Search" className="search"></input>
-            <i style={{ fontSize: "24"}} class="fas fa-search search-icon"></i>
+            <i style={{ fontSize: "24"}} className="fas fa-search search-icon"></i>
         </div>
     );
 };
--- src/Layout/NavBar/NavBar.js
+++ src/Layout/NavBar/NavBar.js
@@ -1,21 +1,75 @@
 import React from "react";
-import Men from "./Men";
-import Ladies from "./Ladies";
-import Girls from "./Girls";
-import Boys from "./Boys";
-
 import "./NavBar.scss";
+
+const categories = [
+    {
+        name: "Men",
+        subCat: [
+            "Tops",
+            "Bottoms",
+            "Dresses",
+            "Jackets",
+            "Shoes",
+            "Accessories",
+            "Sale",
+        ],
+    },
+    {
+        name: "Ladies",
+        subCat: [
+            "Tops",
+            "Bottoms",
+            "Dresses",
+            "Jackets",
+            "Shoes",
+            "Accessories",
+            "Sale",
+        ],
+    },
+    {
+        name: "Girls",
+        subCat: [
+            "Tops",
+            "Bottoms",
+            "Dresses",
+            "Jackets",
+            "Shoes",
+            "Accessories",
+            "Sale",
+        ],
+    },
+    {
+        name: "Boys",
+        subCat: [
+            "Tops",
+            "Bottoms",
+            "Dresses",
+            "Jackets",
+            "Shoes",
+            "Accessories",
+            "Sale",
+        ],
+    },
+];
+
 const NavBar = (props) => {
     return (
         <nav className="text--center nav-bar">
-            <div>
-                <ul>
-                    <Men />
-                    <Ladies />
-                    <Girls />
-                    <Boys />
-                </ul>
-            </div>
+            <ul>
+                {categories.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            {item.name}
+                            <i style={{marginLeft: 5, fontSize: 13}} className="fas fa-chevron-down"></i>
+                            <ul className="sub-menu">
+                                {item.subCat.map((subcat, i) => {
+                                    return <li key={i}>{subcat}</li>;
+                                })}
+                            </ul>
+                        </li>
+                    );
+                })}
+            </ul>
         </nav>
     );
 };
